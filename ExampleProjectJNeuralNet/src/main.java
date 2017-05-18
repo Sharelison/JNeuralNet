fc0919417d3405fd10edf4fb2127497a5f54955a
sharelison caldera <sharelison caldera>
2017-05-18 11:46:21 +0200

    ..

--- ExampleProjectJNeuralNet/src/main.java
+++ ExampleProjectJNeuralNet/src/main.java
@@ -7,7 +7,7 @@ public class main {
 			//Call neuralanalyzer to create & train neuralnet and make predictions.  
 			NeuralAnalyzer na = new NeuralAnalyzer(new MyDataReader(),
 												   new RNeuralNet(0.0, new Integer[]{}, false, "minimal"), //using the Rneuralnet. Caution! threshold (0.0) is a numeric value specifying the threshold for the partial derivatives of the error function as stopping criteria";
-												   "mylibneuralnet.png", //this is the save filepath of the neuralnetwork picture (change it to whatever you want).
+												   "C:/Users/share/Desktop/mylibneuralnet.png", //this is the save filepath of the neuralnetwork picture (change it to whatever you want).
 												   new MyForecaster());
 			na.start(300);
 		} catch (Exception e) {
